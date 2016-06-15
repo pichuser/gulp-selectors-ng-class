b41517d7f868fdd427627429011f7499ad582a6a
pichuser <pichuser@mail.ru>
2016-06-15 19:14:57 +0300

    changes

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-var cheerio = require('cheerio');
-
 module.exports = function (file, classLibrary, idLibrary) {
-    var $ = cheerio.load(file);
-    var c = $('[ng-class]');
-        var allmatches = [];
-        c.each(function(i, el){
-            var t = $(el).attr('ng-class');
-            var r = t.match(/".*?"/g);
-            for (var j in r){
-                allmatches.push(r[j].replace(/"/g, ''));
-            }
-        })
+    var myRegexp = /ng-class="(.*?)"/g;
+    var match = myRegexp.exec(file);
+    var allmatches = [];
+    while (match != null) {
+        console.log(match);
+        var r = match[1].match(/'.*?'/g);
+        for (var j in r){
+            allmatches.push(r[j].replace(/'/g, ''));
+        }
+        match = myRegexp.exec(file);
+    }
     var f = file;
     for(var i in allmatches){
         f = f.replace(new RegExp(allmatches[i], 'g'), function(attr){
